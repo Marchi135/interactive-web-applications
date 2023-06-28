f4ac4d051087fd0c12e7c39d4f5719426b8c0efc
marchi135 <marvelchir@gmail.com>
2023-06-28 21:14:40 +0200

    edit variables removing invalid syntax

--- iwa3/alex.js
+++ iwa3/alex.js
@@ -2,5 +2,5 @@ var firstName = "Alex";
 var surname = "Naidoo";
 var role = "Head of Marketing";
 
-var display= firstname + " " + surname + " (" + role + ")"
+var display= firstname + " " + surname + " (" + role + ")";
 document.querySelector('#alex').innerText = display
