c00d8087c106079aefdc529a2afaca28892546e2
marchi135 <marvelchir@gmail.com>
2023-06-28 21:13:22 +0200

    add new file

---
+++ iwa3/alex.js
@@ -0,0 +1,6 @@
+var firstName = "Alex";
+var surname = "Naidoo";
+var role = "Head of Marketing";
+
+var display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display
