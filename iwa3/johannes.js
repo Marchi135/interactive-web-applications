c8f08e47cb9b51c553673bfc1c302a33e45016f4
marchi135 <marvelchir@gmail.com>
2023-06-28 20:46:47 +0200

    add new js file

---
+++ iwa3/johannes.js
@@ -0,0 +1,6 @@
+const firstName= ('Johannes;')
+const surname= "Potgieter"
+public role = "Intern"
+
+private display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#johannes').innerText = display
