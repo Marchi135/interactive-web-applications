76917fc4b9bc815685b17bbaaddbfd7ba3bc8fe8
marchi135 <marvelchir@gmail.com>
2023-06-28 23:21:01 +0200

    cut files

--- iwa3/johannes.js
+++
@@ -1,6 +0,0 @@
-var firstName= "Johannes";
-var surname= "Potgieter";
-var role = "Intern";
-
-var display = firstName + " " + surname + " (" + role + ")";
-document.querySelector('#johannes').innerText = display;
--- iwa3/nwabisa.js
+++
@@ -1,6 +0,0 @@
-var firstname = "Nwabisa";
-var surname = "Gabe";
-var role = "CEO";
-
-var display = firstname + " " + surname + " (" + role + ")";
-document.querySelector('#nwabisa').innerText = display;
