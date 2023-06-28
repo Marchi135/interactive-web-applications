7360ef5710d22990ac7fbffeada2beea0a89de1c
marchi135 <marvelchir@gmail.com>
2023-06-28 21:24:38 +0200

    update variables

--- iwa3/alex.js
+++ iwa3/alex.js
@@ -2,5 +2,7 @@ var firstName = "Alex";
 var surname = "Naidoo";
 var role = "Head of Marketing";
 
+console.log (firstName, surname, role )
+
 var display= firstname + " " + surname + " (" + role + ")";
 document.querySelector('#alex').innerText = display
